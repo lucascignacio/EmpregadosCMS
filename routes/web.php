7df3a30a9a78ddc48ba089a3510d12702fb7d4a9
Lucas <lucascignacio@gmail.com>
2023-01-13 18:58:54 -0300

    Add Role MVC

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\DepartmentController;
+use App\Http\Controllers\RoleController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -23,3 +24,4 @@ Auth::routes();
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
 Route::resource('departments', DepartmentController::class);
+Route::resource('roles', RoleController::class);
